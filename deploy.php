8ff581b147a224943cfe96bf8e18517fb2ee12be
Alessandro <alessandroporcheddu000@gmail.com>
2025-08-07 14:34:52 +0200

    added deploy config file

---
+++ deploy.php
@@ -0,0 +1,38 @@
+<?php
+namespace Deployer;
+
+require 'recipe/laravel.php';
+
+// Config
+
+set('application', 'rsp-ecommerce');
+set('repository', 'https://github.com/D3stan/rsp-ecom.git');
+set('git_tty', true);
+
+add('shared_files', ['.env']);
+add('shared_dirs', ['storage', 'public/build']);
+add('writable_dirs', ['storage', 'bootstrap/cache']);
+
+// Hosts
+
+host('production')
+    ->set('hostname', 'rsp-industries.com')
+    ->set('remote_user', 'deployer')
+    ->set('deploy_path', '~/ecommerce')
+    ->set('branch', 'main');
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
+
+// Tasks
+
+task('upload:build', function () {
+    upload('public/build', '{{release_path}}/public');
+});
+before('deploy:symlink', 'upload:build');
+
+// Laravel options
+
+set('keep_releases', 3);
+set('default_stage', 'production');
