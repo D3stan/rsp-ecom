01f875e6a2302d885e47638b718923570a032962
Alessandro <alessandroporcheddu000@gmail.com>
2025-11-12 22:59:24 +0100

    removed tmux usage

--- deploy.php
+++ deploy.php
@@ -33,14 +33,7 @@ task('upload:build', function () {
 before('deploy:symlink', 'upload:build');
 
 task('laravel:restart', function () {
-    run("
-        if tmux has-session -t laravel 2>/dev/null; then
-            tmux send-keys -t laravel C-c
-        else
-            tmux new-session -d -s laravel
-        fi
-        tmux send-keys -t laravel 'cd {{release_path}} && php artisan serve' C-m
-    ");
+    run('sudo systemctl restart laravel.service');
 });
 
 task('laravel:optimize', function () {
